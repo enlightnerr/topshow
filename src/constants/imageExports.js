1755bcab311148acb1758d6315cb045ea3985400
Prashant <prashantdubey0098@gmail.com>
2025-08-26 01:07:43 +0530

    updated img exports

--- src/constants/imageExports.js
+++ src/constants/imageExports.js
@@ -1,9 +1,9 @@
-import topshowLogoPng from "@/public/img/topshow.png";
-import topshowLogoFullPngBlack from "@/public/img/topshowFull-black.png";
-import topshowLogoFullPngWhite from "@/public/img/topshowFull-white.png";
-import topshowLogoSvg from "@/public/svg/topshow.svg";
-import topshowLogoFullSvgBlack from "@/public/svg/topshowLogoFull-black.svg";
-import topshowLogoFullSvgWhite from "@/public/svg/topshowLogoFull-white.svg";
+import topshowLogoPng from "../../public/img/topshow.png";
+import topshowLogoFullPngBlack from "../../public/img/topshowFull-black.png";
+import topshowLogoFullPngWhite from "../../public/img/topshowFull-white.png";
+import topshowLogoSvg from "../../public/svg/topshow.svg";
+import topshowLogoFullSvgBlack from "../../public/svg/topshowLogoFull-black.svg";
+import topshowLogoFullSvgWhite from "../../public/svg/topshowLogoFull-white.svg";
 
 export {
   topshowLogoPng,
